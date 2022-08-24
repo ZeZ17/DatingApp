ea27eb7c49ae2f448f0271b11b945d1d7963cc1f
ZeZ17 <89334679+ZeZ17@users.noreply.github.com>
2022-08-24 22:54:30 +0800

    End of Section 12

--- API/Data/Migrations/20220812070255_ExtendedUserEntity.cs
+++ API/Data/Migrations/20220812070255_ExtendedUserEntity.cs
@@ -48,14 +48,14 @@ namespace API.Data.Migrations
                 name: "Interests",
                 table: "Users",
                 type: "TEXT",
-                nullable: false,
+                nullable: true,
                 defaultValue: "");
 
             migrationBuilder.AddColumn<string>(
                 name: "Introduction",
                 table: "Users",
                 type: "TEXT",
-                nullable: false,
+                nullable: true,
                 defaultValue: "");
 
             migrationBuilder.AddColumn<string>(
@@ -76,7 +76,7 @@ namespace API.Data.Migrations
                 name: "LookingFor",
                 table: "Users",
                 type: "TEXT",
-                nullable: false,
+                nullable: true,
                 defaultValue: "");
 
             migrationBuilder.CreateTable(
